7da8da89f619b96add106661df8feb2088ca5404
Alex Wells <autaut03@gmail.com>
2023-10-17 19:08:38 +0300

    fix: Broken serializer interface and adapter not found exception

--- src/GoodPhp/Serialization/Serializer.php
+++ src/GoodPhp/Serialization/Serializer.php
@@ -2,21 +2,16 @@
 
 namespace GoodPhp\Serialization;
 
-use GoodPhp\Reflection\Reflector\Reflection\Attributes\Attributes;
-use GoodPhp\Reflection\Reflector\Reflector;
-use GoodPhp\Reflection\Type\NamedType;
+use GoodPhp\Reflection\Reflection\Attributes\ArrayAttributes;
+use GoodPhp\Reflection\Reflection\Attributes\Attributes;
+use GoodPhp\Reflection\Reflector;
 use GoodPhp\Reflection\Type\Type;
-use GoodPhp\Serialization\TypeAdapter\Registry\TypeAdapterRegistry;
 use GoodPhp\Serialization\TypeAdapter\TypeAdapter;
 use GoodPhp\Serialization\TypeAdapter\TypeAdapterFactory;
 
-final class Serializer
+interface Serializer
 {
-	public function __construct(
-		private readonly TypeAdapterRegistry $typeAdapterRegistry,
-		public readonly Reflector $reflector,
-	) {
-	}
+	public function reflector(): Reflector;
 
 	/**
 	 * @template T
@@ -27,12 +22,5 @@ final class Serializer
 	 *
 	 * @return A
 	 */
-	public function adapter(string $typeAdapterType, Type|string $type, Attributes $attributes = new Attributes(), TypeAdapterFactory $skipPast = null): TypeAdapter
-	{
-		if (is_string($type)) {
-			$type = new NamedType($type);
-		}
-
-		return $this->typeAdapterRegistry->forType($typeAdapterType, $this, $type, $attributes, $skipPast);
-	}
+	public function adapter(string $typeAdapterType, Type|string $type, Attributes $attributes = new ArrayAttributes(), TypeAdapterFactory $skipPast = null): TypeAdapter;
 }
--- src/GoodPhp/Serialization/Serializer/Registry/TypeAdapterNotFoundException.php
+++ src/GoodPhp/Serialization/Serializer/Registry/TypeAdapterNotFoundException.php
@@ -13,7 +13,7 @@ final class TypeAdapterNotFoundException extends RuntimeException
 	public function __construct(string $typeAdapterType, Type $type, Attributes $attributes, ?TypeAdapterFactory $skipPast, string|int $code = 0, Throwable $previous = null)
 	{
 		$message = "A matching type adapter of type '{$typeAdapterType}' for type '{$type}' " .
-			($attributes->count() ? 'with attributes ' . $attributes : '') .
+			($attributes->has() ? 'with attributes ' . $attributes : '') .
 			($skipPast ? 'skipping past ' . get_class($skipPast) . ' ' : '') .
 			'was not found.';
 
