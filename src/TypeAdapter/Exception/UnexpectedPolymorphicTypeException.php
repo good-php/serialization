fab6fa278b5dfadd1e3cbd5e77b542f74b77c144
Alex Wells <autaut03@gmail.com>
2025-05-28 14:55:05 +0300

    Merge pull request #8 from good-php/polymorphic-adapter
    feat: Polymorphic adapter

---
+++ src/TypeAdapter/Exception/UnexpectedPolymorphicTypeException.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace GoodPhp\Serialization\TypeAdapter\Exception;
+
+use RuntimeException;
+use Throwable;
+
+class UnexpectedPolymorphicTypeException extends RuntimeException implements UnexpectedValueException
+{
+	/**
+	 * @param list<string> $expectedTypeNames
+	 */
+	public function __construct(
+		public readonly string $typeNameField,
+		public readonly string $value,
+		public readonly array $expectedTypeNames,
+		Throwable $previous = null
+	) {
+		parent::__construct(
+			"Only the following polymorphic types for field '{$this->typeNameField}' are allowed: [" .
+				implode(', ', $this->expectedTypeNames) .
+				"], but got '{$this->value}'",
+			0,
+			$previous
+		);
+	}
+}
